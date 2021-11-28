36f3e00875019f2cf8161b2d2832157dcb7611ff
Prasun117 <57390215+Prasun117@users.noreply.github.com>
2021-11-28 16:52:51 +0530

    Added Login screen

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,6 +1,48 @@
 import React from "react";
-import { Grid, Paper, Card, CardHeader, CardContent } from "@material-ui/core";
+import {
+  Avatar,
+  Grid,
+  Paper,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  makeStyles,
+  Divider,
+  TextField,
+  CardMedia,
+  Button,
+  CardActions
+} from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
+import PersonIcon from "@material-ui/icons/Person";
+import image from "../assets/contemplative-reptile.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[900]
+  }
+}));
+
 export const LoginForm = (props) => {
+  const classes = useStyles();
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
@@ -8,7 +50,7 @@ export const LoginForm = (props) => {
   return (
     <div className="login-form">
       <Grid container>
-        <Grid item xs={6}>
+        <Grid item md={6} sm={12} lg={6}>
           <Paper>
             <form onSubmit={handleSubmit}>
               UserName:
@@ -20,21 +62,41 @@ export const LoginForm = (props) => {
           </Paper>
         </Grid>
 
-        <Grid item xs={6}>
+        <Grid item md={6} sm={12} lg={6} xs={12}>
           <Card>
-            <CardHeader></CardHeader>
-            <CardContent></CardContent>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  <PersonIcon />
+                </Avatar>
+              }
+              title="User Login"
+            ></CardHeader>
+            <Divider />
+            <CardContent>
+              <Typography>UserName:</Typography>
+              <TextField
+                id="username"
+                label="User name"
+                variant="outlined"
+                fullWidth={true}
+              />
+              <Typography>Password:</Typography>
+              <TextField
+                id="username"
+                label="Password"
+                variant="outlined"
+                fullWidth={true}
+              />
+            </CardContent>
+            <CardActions disableSpacing>
+              <Button color="primary" size="medium" variant="contained">
+                Submit
+              </Button>
+            </CardActions>
           </Card>
         </Grid>
       </Grid>
-
-      {/* <form onSubmit={handleSubmit}>
-        UserName:
-        <input type="text" />
-        Password:
-        <input type="text" />
-        <button type="submit">submit</button>
-      </form> */}
     </div>
   );
 };
